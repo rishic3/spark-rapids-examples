d4317d757ebf7a43ca52a4255b47bc1576fdf933
Rishi Chandra <rishic@nvidia.com>
2025-03-11 11:00:09 -0700

    updates

--- examples/ML+DL-Examples/Spark-DL/dl_inference/llm_benchmarks/bench_llm_spark.py
+++ examples/ML+DL-Examples/Spark-DL/dl_inference/llm_benchmarks/bench_llm_spark.py
@@ -20,6 +20,9 @@ from pytriton_utils import TritonServerManager
 # no plugin:
 # spark-submit --properties-file spark-llm-config.conf bench_llm_spark.py
 
+# triton benchmark:
+# spark-submit --properties-file spark-llm-config-16-core.conf bench_llm_spark.py
+
 def triton_server(ports):
     import time
     import signal
@@ -31,8 +34,8 @@ def triton_server(ports):
     from vllm import LLM, SamplingParams
 
     print(f"SERVER: Initializing model on worker {TaskContext.get().partitionId()}.")
-    sampling_params = SamplingParams(temperature=0.7, top_p=0.8, repetition_penalty=1.05, max_tokens=256)
-    llm = LLM(model="Qwen/Qwen2.5-7B-Instruct", dtype="bfloat16")
+    sampling_params = SamplingParams(temperature=0.7, top_p=0.8, repetition_penalty=1.05, max_tokens=128)
+    llm = LLM(model="Qwen/Qwen2.5-3B-Instruct", dtype="bfloat16", max_num_seqs=512, max_num_batched_tokens=400000)
 
     @batch
     def _infer_fn(**inputs):
@@ -56,8 +59,8 @@ def triton_server(ports):
                 Tensor(name="outputs", dtype=object, shape=(-1,)),
             ],
             config=ModelConfig(
-                max_batch_size=64,
-                batcher=DynamicBatcher(max_queue_delay_microseconds=5000),  # 5ms
+                max_batch_size=512,
+                batcher=DynamicBatcher(max_queue_delay_microseconds=50000),  # 50ms
             ),
             strict=True,
         )
@@ -75,20 +78,20 @@ def triton_fn(model_name, host_to_url):
     from pytriton.client import ModelClient
     import socket
 
+
     url = host_to_url.get(socket.gethostname())
-        
+    client = ModelClient(url, model_name, inference_timeout_s=500)
     print(f"Connecting to Triton model {model_name} at {url}.")
 
     def infer_batch(inputs):
-        with ModelClient(url, model_name, inference_timeout_s=500) as client:
-            flattened = np.squeeze(inputs).tolist()
-            # Encode batch
-            encoded_batch = [[text.encode("utf-8")] for text in flattened]
-            encoded_batch_np = np.array(encoded_batch, dtype=np.bytes_)
-            # Run inference
-            result_data = client.infer_batch(encoded_batch_np)
-            result_data = np.squeeze(result_data["outputs"], -1)
-            return result_data
+        flattened = np.squeeze(inputs).tolist()
+        # Encode batch
+        encoded_batch = [[text.encode("utf-8")] for text in flattened]
+        encoded_batch_np = np.array(encoded_batch, dtype=np.bytes_)
+        # Run inference
+        result_data = client.infer_batch(encoded_batch_np)
+        result_data = np.squeeze(result_data["outputs"], -1)
+        return result_data
         
     return infer_batch
 
@@ -108,6 +111,9 @@ def main():
     model_name = "qwen-2.5"
     num_nodes = 1
     server_manager = TritonServerManager(num_nodes, model_name)
+
+    start_server_time = time.perf_counter()
+
     server_manager.start_servers(triton_server, wait_timeout=24)
     host_to_http_url = server_manager.host_to_http_url
 
@@ -115,13 +121,13 @@ def main():
 
     # Define system prompt and predict function
     system_prompt = '''You are a knowledgeable AI assistant. Your job is to create a 2-3 sentence summary 
-of a research abstract that captures the main objective, methodology, and key findings, using clear 
+of a research abstract that captures the main objective, methodology, and key findings, using clear and concise
 language while preserving technical accuracy and quantitative results.'''
 
     generate = predict_batch_udf(partial(triton_fn, model_name=model_name, host_to_url=host_to_http_url),
                                 return_type=StringType(),
                                 input_tensor_shapes=[[1]],
-                                batch_size=64)
+                                batch_size=256)
     
     # Start monitoring
     monitor = GPUMonitor()
@@ -151,6 +157,7 @@ language while preserving technical accuracy and quantitative results.'''
         # Logging
         logtime = monitor.log_file.split(".")[-2].split("metrics")[1]
         print(f"E2E read -> inference -> write time: {end_write - start_read:.4f} seconds")
+        print(f"Triton server start time: {start_read - start_server_time:.4f} seconds")
         parent_dir = "/home/rishic/spark-rapids-examples/examples/ML+DL-Examples/Spark-DL/dl_inference/llm_benchmarks/results"
         with open(f"{parent_dir}/out_times_{log_prefix}{logtime}.txt", "w") as f:
             f.write(f"Start read time: {start_read}\nEnd write time: {end_write}\nNum partitions: {num_parts}\n")
